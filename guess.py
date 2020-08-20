a5d6ee85bd0083e22a584063bb749c3da55b9912
SungChain Chang <ga846014@gmail.com>
2020-08-20 12:54:21 +0800

    version2

--- guess.py
+++ guess.py
@@ -1,13 +1,17 @@
 import random
 ans = random.randint(1, 100)
-print('答案: ', ans)
+#print('答案: ', ans)
+i = 0
 while ans > 0:
     a = input('請輸入數字(1~100): ')
     a = int(a)
+    i += 1 # i = i + 1
     if a > ans:
 	    print('猜錯囉!比答案大')
     elif a < ans:
 	    print('猜錯囉!比答案小')
     else:
 	    print('答對了!')
+	    print('這是你猜的第', i,'次')
 	    break
+    print('這是你猜的第', i,'次')
