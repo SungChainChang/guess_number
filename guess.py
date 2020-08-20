22878c7d271f9ccc8a717642a0299e602c045f07
SungChain Chang <ga846014@gmail.com>
2020-08-20 13:01:50 +0800

    version finall

--- guess.py
+++ guess.py
@@ -1,9 +1,14 @@
 import random
-ans = random.randint(1, 100)
+small = input('請輸入最小值: ')
+big = input('請輸入最大值: ')
+small = int(small)
+big = int(big)
+
+ans = random.randint(small, big)
 #print('答案: ', ans)
 i = 0
-while ans > 0:
-    a = input('請輸入數字(1~100): ')
+while True:
+    a = input('請輸入數字: ')
     a = int(a)
     i += 1 # i = i + 1
     if a > ans:
